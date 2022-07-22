e9da83517e5fab0390fef5ad006546b42481bff6
sisay2405 <abrehasisay04@gmail.comgit config --global user.name sisay2405egit config --global user.email abrehasisay04@gmail.com>
2022-07-22 02:46:30 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Fullstack React Blog Project-------
+
+## Description:
+ This app build with the existing knowledge of the React basics, and give you further practice working with codebase linting, component testing, Redux Toolkit, and TypeScript. 
+
+## wireframe
+
+## Demo:
+
+## applications installed
+---------
+## ....
+# fullstack-react-blog-app
---
+++ server/index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose');
+const mongoSTRING = process.env.DATABASE_URL 
+const routes = require('./routes/routes')
+
+mongoose.connect(mongoSTRING)
+const database = mongoose.connection
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+const app = express();
+app.use(express.json());
+app.use('/api', routes)
+
+app.listen(7000, () => {
+    console.log(`Server Started at ${7000}`)
+})
