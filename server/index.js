c0dae393ac5d23c0181c79c79cf316401f0d2a13
sisay2405 <abrehasisay04@gmail.comgit config --global user.name sisay2405egit config --global user.email abrehasisay04@gmail.com>
2022-08-04 17:37:03 -0400

    server side fix

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
+cors = require
 const mongoSTRING = process.env.DATABASE_URL 
 const routes = require('./routes/routes')
 
@@ -18,7 +19,7 @@ database.once('connected', () => {
 const app = express();
 app.use(express.json());
 app.use('/api', routes)
-
+app.use(cors)
 app.listen(4000, () => {
     console.log(`Server Started at ${4000}`)
 })
